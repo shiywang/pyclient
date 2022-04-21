156aa42c0ab448bed9fa66a6922b7be83f9f0f5c
Shiyang Wang <shiyangwang@umass.edu>
2022-04-21 15:41:34 -0400

    update to make azure work

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
-auth_token = 'd414d49731d46ce1ce40bf9350ed5bf9544014de'
+auth_token = '96ccb7d9451ec41b99bff50bb3b108fe0ef9fa41'
 request_headers = {'Content-Type': 'application/json', 'Authorization': 'Token '+ auth_token}
 test_device_id      = "F43053011ACF"
 test_device_type    = "RRI"
 filename = "./data_store/test.txt"
 
 
-base_ip = "nelab.ddns.umass.edu"
-mqtt_port = "30482"
+base_ip = "nelab-ingress.eastus.cloudapp.azure.com"
+mqtt_port = "9000"
 
 base_url = "http://" + base_ip + "/"
